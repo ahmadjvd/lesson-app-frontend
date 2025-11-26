3ec57ee624213139d0364f91071e114461ac0733
ahmadjvd <ahmadjvd23@gmail.com>
2025-11-26 18:06:50 +0400

    vue instance created

---
+++ index.js
@@ -0,0 +1,15 @@
+
+  let app = new Vue({
+    el: '#app',
+    data: {
+      lessons: [],
+      sortAttribute: 'subject',
+      sortOrder: 'asc',
+      cart: [],
+      currentPage: 'lesson',
+      name: '',
+      phone: '',
+      orderConfirmed: false,
+      searchQuery: '',
+      isFormValid: false
+  }});
